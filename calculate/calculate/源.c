18c39b3c923da0da7dbc369aef34aeb32380db21
blackcurrents <193507779@qq.com>
2023-01-12 23:25:10 +0800

    阶乘之和

--- calculate/calculate/源.c
+++ calculate/calculate/源.c
@@ -1,22 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//The old version
+//int main()
+//{
+//	int net = 0;
+//	int x = 0;
+//	int sum = 0;
+//	printf("enter one number:");
+//	scanf("%d", &net);
+//	for (x = 1; x < net + 1; x++)
+//	{
+//		int i = 0;
+//		int num = 1;
+//		for (i = 1; i < x + 1; i++)
+//		{
+//			num = num * i;
+//		}
+//		sum += num;
+//	}
+//	printf("result is:%d", sum);
+//	return 0;
+//}
+
+
+//The New Version
 int main()
 {
-	int net = 0;
-	int x = 0;
 	int sum = 0;
-	printf("enter one number:");
-	scanf("%d", &net);
-	for (x = 1; x < net + 1; x++)
+	int i = 0;
+	int n = 0;
+	int ret = 1;
+	printf("enter a number:");
+	scanf("%d", &n);
+	for (i = 1; i < n + 1; i++)
 	{
-		int i = 0;
-		int num = 1;
-		for (i = 1; i < x + 1; i++)
-		{
-			num = num * i;
-		}
-		sum += num;
+		ret *= i;
+		sum += ret;
 	}
-	printf("result is:%d", sum);
+	printf("result:%d",sum);
 	return 0;
 }
